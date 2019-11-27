d35381d948d55398ba5e1ed31897368c59518691
Michal Szczepaniak <michal.szczepaniak@inspirelabs.pl>
2019-11-27 10:59:07 +0100

    add script to html - local works

--- script/script.js
+++ script/script.js
@@ -1,6 +1,10 @@
-console.log("działa");
+// console.log("działa");
+//
+// const p = document.createElement('p');
+// p.textContent = 'Hello word!';
+// window.document.body.appendChild(p);
 
-const p = document.createElement('p');
-p.textContent = 'Hello word!';
-window.document.body.appendChild(p);
+
+
+
 
