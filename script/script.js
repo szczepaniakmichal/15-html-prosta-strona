aaa2ae2ce765bea2ba403dc9d415800f97c1067d
Michal Szczepaniak <michal.szczepaniak@inspirelabs.pl>
2019-11-29 10:21:29 +0100

    @media portrait WAZNY KOD

--- script/script.js
+++ script/script.js
@@ -5,6 +5,26 @@
 // window.document.body.appendChild(p);
 
 
+const checkk = () => {
+    let getEl = [...document.querySelectorAll("input")];
+    // console.log(getEl);
+    let howManyInputs = 0;
+
+    getEl.forEach(e => {
+        if (e.checked) {
+            e.checked = false;
+        }
+        else if (e.checked === false) {
+            e.checked = true;
+        }
+        if (e.value === '') {
+            e.value = "przykładowy text";
+        }
+        howManyInputs++;
+    });
+    console.log(howManyInputs);
+};
+
 
 
 
