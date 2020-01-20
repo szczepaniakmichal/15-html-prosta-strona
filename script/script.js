4aea0a5442abc16a90ca0a3e108a9d1f61f7d31d
szczepaniakmichal <mssk87@gmail.com>
2020-01-20 21:46:10 +0100

    dynamic add file with style

--- script/script.js
+++ script/script.js
@@ -1,51 +1,15 @@
-// console.log("działa");
-//
-// const p = document.createElement('p');
-// p.textContent = 'Hello word!';
-// window.document.body.appendChild(p);
+console.log('jest')
 
-
-const checkk = () => {
-    let getEl = [...document.querySelectorAll("input")];
-    // console.log(getEl);
-    let howManyInputs = 0;
-
-    getEl.forEach(e => {
-        if (e.checked) {
-            e.checked = false;
-        }
-        else if (e.checked === false) {
-            e.checked = true;
-        }
-        if (e.value === '') {
-            e.value = "przykładowy text";
-        }
-        howManyInputs++;
-    });
-    console.log(howManyInputs);
-};
-
-
-
-document.querySelector("#content-dane-fatca-8768550871eca148a120addb7ff2b478 > form > div > div.panel-navigation.align-bottom.text-right > button.btn.btn-submit.btn-dalej").addEventListener("click", function() {console.log("bleeeee"));
-
-function() {
-    alert('I am anonymous');
-}
-
-const zzz = function() {
-    alert('I am anonymous');
-};
-
-zzz()
-
-document.querySelector("#content-dane-fatca-8768550871eca148a120addb7ff2b478 > form > div > div.panel-navigation.align-bottom.text-right > button.btn.btn-submit.btn-dalej").addEventListener("click", zzz);
-
-
-
-
-
-
+setTimeout(() => {
+    let head  = document.getElementsByTagName('head')[0];
+    let link  = document.createElement('link');
+    // link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'css/style.css';
+    link.media = 'all';
+    head.appendChild(link);
+}, 8000);
 
 
 
