bafaf8f798d37a96cf842320a6ef026e24984e0f
szczepaniakmichal <mssk87@gmail.com>
2020-04-18 20:24:44 +0200

    test dddddddddddd

--- script/script.js
+++ script/script.js
@@ -14,3 +14,6 @@ Array.prototype.slice.call(document.querySelectorAll('th')).forEach(function(th)
         .forEach(function(tr) { table.appendChild(tr) });
 })
 });
+
+
+console.log("ddddddddddddddd")
